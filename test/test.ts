f7003eb54fe35fa859326c94c971a8ff8827c117
Vadim Chumak <vadim.inc@gmail.com>
2021-04-22 14:25:25 +0200

    improve test for duplicated key

--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,6 @@
 import { ethers } from 'hardhat';
 import { expect } from 'chai';
+import * as chaiAsPromised from "chai-as-promised";
 
 let Contract;
 let contract;
@@ -21,7 +22,10 @@ describe('Operators', function() {
 
   // Test case
   it('Revert adding new operator with same pubkey', async function () {
-    await contract.addOperator('stakefish', '0x7d6d6C319b0dE2841bB0E564300D0dE4D8B0f403', '10', '0x8b3d89d1bdb347e194b220201507c43de971ee1e'); 
+    // Store new
+    await contract.addOperator('stakefish', '0x7d6d6C319b0dE2841bB0E564300D0dE4D8B0f403', '1000', '0xe52350A8335192905359c4c3C2149976dCC3D8bF');
+    // Try to sttore with duplicated public key
+    await expect(contract.addOperator('stakefish', '0x7d6d6C319b0dE2841bB0E564300D0dE4D8B0f403', '10', '0x8b3d89d1bdb347e194b220201507c43de971ee1e')).to.be.throw;
     // Note that we need to use strings to compare the 256 bit integers
     expect((await contract.operatorCount()).toString()).to.equal('1');
   });
