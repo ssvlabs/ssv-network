591795016b9479fcfa34e2620ecc5ebea82254fd
Vadim Chumak <vadim.inc@gmail.com>
2021-04-22 11:37:34 +0200

    minor changes

--- test/test.ts
+++ test/test.ts
@@ -4,7 +4,7 @@ import { expect } from 'chai';
 let Contract;
 let contract;
 
-describe('SSVNetworkV', function() {
+describe('Operators', function() {
   beforeEach(async function () {
     Contract = await ethers.getContractFactory('SSVNetwork');
     contract = await Contract.deploy();
@@ -12,7 +12,7 @@ describe('SSVNetworkV', function() {
   });
  
   // Test case
-  it('retrieve returns a value total operators', async function () {
+  it('Add new operator', async function () {
     // Store a value
     await contract.addOperator('stakefish', '0x7d6d6C319b0dE2841bB0E564300D0dE4D8B0f403', '1000', '0xe52350A8335192905359c4c3C2149976dCC3D8bF'); 
     // Note that we need to use strings to compare the 256 bit integers
