7ba58e5338d6898a5a518a207b01e62f9d4e1a3b
Vadim Chumak <vadim.inc@gmail.com>
2021-05-17 10:40:20 +0200

    add code coverage

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,6 +7,7 @@ import '@nomiclabs/hardhat-etherscan';
 import '@openzeppelin/hardhat-upgrades';
 import "hardhat-gas-reporter";
 import "hardhat-tracer";
+import "solidity-coverage";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
