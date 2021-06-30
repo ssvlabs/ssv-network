2c4363b93e8c223cbb484e4cfd0a4dda53ed65ef
Adam Zigdon <adam.zigdon@gmail.com>
2021-06-30 12:14:32 +0300

    update DEX

--- contracts/DEX.sol
+++ contracts/DEX.sol
@@ -4,9 +4,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract DEX {
     event CDTToSSVConverted(address sender, uint256 cdtAmount, uint256 ssvAmount);
-    event SSVToCDTConverted(address sender, uint256 ssvAmount, uint256 cdtAmount);
-
-    mapping(address => uint256) public cdtAllowance;
 
     IERC20 public cdtToken;
     IERC20 public ssvToken;
@@ -24,19 +21,6 @@ contract DEX {
         uint256 cdtAmount = ssvAmount * rate;
         cdtToken.transferFrom(msg.sender, address(this), cdtAmount);
         ssvToken.transfer(msg.sender, ssvAmount);
-        cdtAllowance[msg.sender] += cdtAmount;
         emit CDTToSSVConverted(msg.sender, cdtAmount, ssvAmount);
-    }
-
-    function convertSSVToCDT(uint256 _amount) public {
-        uint256 cdtAmount = _amount * rate;
-        require(
-            cdtAllowance[msg.sender] >= cdtAmount,
-            "DEX: Can't exceed original CDT amount"
-        );
-        cdtAllowance[msg.sender] -= cdtAmount;
-        ssvToken.transferFrom(msg.sender, address(this), _amount);
-        cdtToken.transfer(msg.sender, cdtAmount);
-        emit SSVToCDTConverted(msg.sender, _amount, cdtAmount);
     }
 }
