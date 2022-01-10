0c8dc708062e0fed5126505feaf3f76d52f31c8c
Vadim <vadim.inc@gmail.com>
2022-01-10 14:02:30 +0100

    upgraded contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -60,7 +60,7 @@ if (process.env.GOERLI_ETH_NODE_URL) {
   config.networks['goerli'] = {
     url: process.env.GOERLI_ETH_NODE_URL,
     accounts: [`0x${process.env.GOERLI_OWNER_PRIVATE_KEY}`],
-    gasPrice: process.env.GAS_PRICE
+    gasPrice: +process.env.GAS_PRICE
   }
 }
 module.exports = config;
