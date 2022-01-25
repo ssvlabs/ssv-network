b493c26060cc9e1e08fd5435f71d7962ab1b9cdb
olegshmuelov <45327364+olegshmuelov@users.noreply.github.com>
2022-01-25 15:14:31 +0200

    build & compare to latest & print report

--- incentivized_testnet/cmd.js
+++ incentivized_testnet/cmd.js
@@ -68,8 +68,8 @@ async function exportEventsData(dataType, fromBlock, latestBlock) {
 }
 
 async function extractOperatorsWithMetrics(operators, validatorsWithMetrics, operatorsDecided, verifiedOperators) {
-    let rawdata = fs.readFileSync('ghost_operators.json');
-    let ghostList = JSON.parse(rawdata)['data'];
+    let rawData = fs.readFileSync(`${__dirname}/ghost_operators.json`);
+    let ghostList = JSON.parse(rawData)['data'];
 
     return operators.reduce((aggr, operator) => {
         const validators = validatorsWithMetrics.filter((validator) => {
@@ -181,6 +181,149 @@ async function getEventDetails(events, dataType) {
     );
 }
 
+function buildReport(operatorsWithMetrics, validatorsWithMetrics, fromEpoch, toEpoch) {
+    const attrAvg = (v, prop = 'attestations') => (v.reduce((a, b) => a + +b[prop], 0) / v.length);
+
+    const operatorsActive = operatorsWithMetrics.filter((operator) => {
+        return operator.active === "true"
+    });
+    const operatorsActiveNotNaN = operatorsActive.filter((operator) => {
+        return operator.attestationsWithout0 !== "NaN"
+    });
+    const operatorsActiveGte90 = operatorsActiveNotNaN.filter((validator) => {
+        return +validator.attestationsWithout0 >= 90
+    });
+    const validators = validatorsWithMetrics.filter((validator) => {
+        return validator.active === "true"
+    });
+    const validatorsShouldAttest = validators.filter((validator) => {
+        return validator.shouldAttest === "true"
+    });
+    const validatorsShouldAttest0 = validatorsShouldAttest.filter((validator) => {
+        return +validator.attestations === 0
+    });
+    const validatorsShouldAttestNon0 = validatorsShouldAttest.filter((validator) => {
+        return +validator.attestations !== 0
+    });
+    const validatorsShouldAttestGte90 = validatorsShouldAttest.filter((validator) => {
+        return +validator.attestations >= 90
+    });
+
+    // todo: handle divide by 0
+    let metrics = {
+        operator: {
+            total: operatorsWithMetrics.length,
+            active: operatorsActive.length,
+            active_per: operatorsActive.length / operatorsWithMetrics.length * 100,
+            attr_avg: attrAvg(operatorsActiveNotNaN, 'attestationsWithout0'),
+            active_gte_90: operatorsActiveGte90.length,
+            active_gte_90_per: operatorsActiveGte90.length / operatorsActive.length * 100,
+            attr_avg_gte_90: attrAvg(operatorsActiveGte90, 'attestationsWithout0'),
+        },
+        validator: {
+            active: validators.length,
+            should_attest: validatorsShouldAttest.length,
+            should_attest_per: validatorsShouldAttest.length / validators.length * 100,
+            should_attest_0: validatorsShouldAttest0.length,
+            should_attest_0_per: validatorsShouldAttest0.length / validatorsShouldAttest.length * 100,
+            attr_avg: attrAvg(validatorsShouldAttestNon0),
+            should_attest_gte_90: validatorsShouldAttestGte90.length,
+            should_attest_gte_90_per: validatorsShouldAttestGte90.length / validatorsShouldAttest.length * 100,
+            attr_avg_gte_90: attrAvg(validatorsShouldAttestGte90)
+        }
+    }
+
+    let latestMetrics
+    const latestMetricsFile = `${__dirname}/latest_metrics.json`;
+    try {
+        const latestMetricsFs = fs.readFileSync(latestMetricsFile);
+        latestMetrics = JSON.parse(latestMetricsFs.toString());
+    } catch {
+        console.log(`${latestMetricsFile} file not found`)
+    }
+
+    if (latestMetrics) {
+        metrics.operator.total_chg = changeValueWithSIgn(metrics.operator.total - latestMetrics.operator.total);
+        metrics.operator.active_chg = changeValueWithSIgn(metrics.operator.active - latestMetrics.operator.active);
+        metrics.operator.attr_avg_chg = changeValueWithSIgn(metrics.operator.attr_avg - latestMetrics.operator.attr_avg, 2);
+        metrics.operator.active_gte_90_chg = changeValueWithSIgn(metrics.operator.active_gte_90 - latestMetrics.operator.active_gte_90);
+        metrics = buildMetricsChgValues(metrics, latestMetrics, "validator");
+    }
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    console.log(`daily ssv.network stats (prater: ${fromEpoch}-${toEpoch})`)
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    console.log("")
+    console.log(`Operator Stats (Total: ${metrics.operator.total}${latestMetrics ? "[".concat(metrics.operator.total_chg, "]") : ""})`);
+    console.log(` - ${metrics.operator.active_per.toFixed()}% (${metrics.operator.active}${latestMetrics ? "[".concat(metrics.operator.active_chg, "]") : ""}) of operators are active (decided in the last 100 epochs)`);
+    console.log(` - ${metrics.operator.attr_avg.toFixed(2)}% ${latestMetrics ? "[".concat(metrics.operator.attr_avg_chg, "%]") : ""} attr.avg (active operators excluding 0 attr.avg)`);
+    console.log(` - ${metrics.operator.active_gte_90_per.toFixed()}% (${metrics.operator.active_gte_90}${latestMetrics ? "[".concat(metrics.operator.active_gte_90_chg, "]") : ""}) from the active are >=90% attr.avg with ${metrics.operator.attr_avg_gte_90.toFixed(2)}% attr.avg`);
+    printValidatorsStats("Validator Stats", metrics, "validator", latestMetrics)
+
+    const customOperatorPublicKey = process.env.CUSTOM_OPERATOR_PUBLIC_KEY;
+    if (customOperatorPublicKey) {
+        const customFiltered = (v) => v.filter((val) => {
+            return val.operatorPublicKeys.includes(customOperatorPublicKey)
+        });
+
+        const customValidators = customFiltered(validators);
+        const customValidatorsShouldAttest = customFiltered(validatorsShouldAttest);
+        const customValidatorsShouldAttest0 = customFiltered(validatorsShouldAttest0);
+        const customValidatorsShouldAttestNon0 = customFiltered(validatorsShouldAttestNon0);
+        const customValidatorsShouldAttestGte90 = customFiltered(validatorsShouldAttestGte90);
+
+        if (customValidators.length > 0) {
+            metrics.custom = {
+                public_key: customOperatorPublicKey,
+                active: customValidators.length,
+                should_attest: customValidatorsShouldAttest.length,
+                should_attest_per: customValidatorsShouldAttest.length / customValidators.length * 100,
+                should_attest_0: customValidatorsShouldAttest0.length,
+                should_attest_0_per: customValidatorsShouldAttest0.length / customValidatorsShouldAttest.length * 100,
+                attr_avg: attrAvg(customValidatorsShouldAttestNon0),
+                should_attest_gte_90: customValidatorsShouldAttestGte90.length,
+                should_attest_gte_90_per: customValidatorsShouldAttestGte90.length / customValidatorsShouldAttest.length * 100,
+                attr_avg_gte_90: attrAvg(customValidatorsShouldAttestGte90)
+            }
+            if (latestMetrics) {
+                //todo handle different custom operators
+                metrics = buildMetricsChgValues(metrics, latestMetrics, "custom");
+            }
+            const customOperatorName = operatorsWithMetrics.find(obj => {
+                return obj.publicKey === process.env.CUSTOM_OPERATOR_PUBLIC_KEY
+            }).name;
+            printValidatorsStats(`${customOperatorName} Operator's Validator Stats`, metrics, "custom", latestMetrics)
+        }
+    }
+    fs.writeFile(latestMetricsFile, JSON.stringify(metrics, null, 4), function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
+
+function changeValueWithSIgn(change, toFix = 0) {
+    return `${change >= 0 ? "+" : ""}${change.toFixed(toFix)}`
+}
+
+function buildMetricsChgValues(metrics, latestMetrics, type) {
+    metrics[type].active_chg = changeValueWithSIgn(metrics[type].attr_avg - latestMetrics[type].attr_avg);
+    metrics[type].should_attest_chg = changeValueWithSIgn(metrics[type].should_attest - latestMetrics[type].should_attest);
+    metrics[type].should_attest_0_chg = changeValueWithSIgn(metrics[type].should_attest_0 - latestMetrics[type].should_attest_0);
+    metrics[type].attr_avg_chg = changeValueWithSIgn(metrics[type].attr_avg - latestMetrics[type].attr_avg, 2);
+    metrics[type].should_attest_gte_90_chg = changeValueWithSIgn(metrics[type].should_attest_gte_90 - latestMetrics[type].should_attest_gte_90);
+    return metrics
+}
+
+function printValidatorsStats(title, metrics, type, latestMetrics) {
+    console.log("")
+    console.log(`---------------------------------------------------------------------------`);
+    console.log(`${title} (Total: ${metrics[type].active}${latestMetrics ? "[".concat(metrics[type].active_chg, "]") : ""})`);
+    console.log(` - ${metrics[type].should_attest_per.toFixed()}% (${metrics[type].should_attest}${latestMetrics ? "[".concat(metrics[type].should_attest_chg, "]") : ""}) "should attest" (3 active operators at least)`);
+    console.log(` - ${metrics[type].should_attest_0_per.toFixed()}% (${metrics[type].should_attest_0}${latestMetrics ? "[".concat(metrics[type].should_attest_0_chg, "]") : ""}) of the "should attest" validators are not attesting (0 attr.avg)`);
+    console.log(` - ${metrics[type].attr_avg.toFixed(2)}% ${latestMetrics ? "[".concat(metrics[type].attr_avg_chg, "%]") : ""} attr.avg ("should attest" excluding 0 attr.avg)`);
+    console.log(` - ${metrics[type].should_attest_gte_90_per.toFixed()}% (${metrics[type].should_attest_gte_90}${latestMetrics ? "[".concat(metrics[type].should_attest_gte_90_chg, "]") : ""}) from the "should attest" are >=90% attr.avg with ${metrics[type].attr_avg_gte_90.toFixed(2)}% attr.avg`);
+}
+
 async function fetch() {
     const cacheFile = `${__dirname}/.process.cache`;
     fs.stat(cacheFile, async (err, stat) => {
@@ -200,6 +343,9 @@ async function fetchValidatorMetrics(fromEpoch, toEpoch) {
     await pgConnect();
     const operatorsFile = `${__dirname}/operators.csv`;
     const validatorsFile = `${__dirname}/validators.csv`;
+    // used to calculate the stats directly from the csv
+    // const operatorsFile = `${__dirname}/operators_extra_67790-67890.csv`;
+    // const validatorsFile = `${__dirname}/validators_extra_67790-67890.csv`;
     await stat(validatorsFile);
     await stat(operatorsFile);
 
@@ -229,6 +375,10 @@ async function fetchValidatorMetrics(fromEpoch, toEpoch) {
     const operatorsDecided = await extractOperatorsDecided(fromEpoch, toEpoch);
     const validatorsWithMetrics = await extractValidatorsWithMetrics(validators, operators, operatorsDecided, fromEpoch, toEpoch);
     const operatorsWithMetrics = await extractOperatorsWithMetrics(operators, validatorsWithMetrics, operatorsDecided, verifiedOperators);
+    // used to calculate the stats directly from the csv
+    // const validatorsWithMetrics = validators;
+    // const operatorsWithMetrics = operators;
+    buildReport(operatorsWithMetrics, validatorsWithMetrics, fromEpoch, toEpoch)
 
     stringify(validatorsWithMetrics, {
         header: true
@@ -249,7 +399,7 @@ async function fetchValidatorMetrics(fromEpoch, toEpoch) {
 
 function extractOperatorsDecided(fromEpoch, toEpoch) {
     const res = client.search({
-        index: 'decided_search',
+        index: 'decided_search_v2_71600',
         body: {
             query: {
                 range: {
