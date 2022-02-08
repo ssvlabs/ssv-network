fc1036deed579cada011f2f0c9025c54ac751d76
Vadim <vadim.inc@gmail.com>
2022-02-08 11:43:05 +0100

    deploy data

--- scripts/ssv-deploy-test.ts
+++ scripts/ssv-deploy-test.ts
@@ -1,30 +1,30 @@
 import { ethers, upgrades } from 'hardhat';
 
 async function main() {
-  console.log('Deploying OLDToken...');
-  const oldTokenFactory = await ethers.getContractFactory('OldTokenMock');
-  const oldToken = await oldTokenFactory.deploy();
-  await oldToken.deployed();
-  console.log('Deploying SSVToken...');
-  const ssvTokenFactory = await ethers.getContractFactory('SSVToken');
-  const ssvToken = await ssvTokenFactory.deploy();
-  await ssvToken.deployed();
-  console.log('Deploying DEX...');
-  const dexFactory = await ethers.getContractFactory('DEX');
-  const dex = await upgrades.deployProxy(
-    dexFactory,
-    [oldToken.address, ssvToken.address, 100]
-  );
-  await dex.deployed();
+  // console.log('Deploying OLDToken...');
+  // const oldTokenFactory = await ethers.getContractFactory('OldTokenMock');
+  // const oldToken = await oldTokenFactory.deploy();
+  // await oldToken.deployed();
+  // console.log('Deploying SSVToken...');
+  // const ssvTokenFactory = await ethers.getContractFactory('SSVToken');
+  // const ssvToken = await ssvTokenFactory.deploy();
+  // await ssvToken.deployed();
+  // console.log('Deploying DEX...');
+  // const dexFactory = await ethers.getContractFactory('DEX');
+  // const dex = await upgrades.deployProxy(
+  //   dexFactory,
+  //   [oldToken.address, ssvToken.address, 100]
+  // );
+  // await dex.deployed();
   console.log('Deploying SSVRegistry...');
   const ssvRegistryFactory = await ethers.getContractFactory('SSVRegistry');
   const ssvRegistry = await upgrades.deployProxy(ssvRegistryFactory, { initializer: false });
   await ssvRegistry.deployed();
   console.log('Deploying SSVNetwork...');
   const ssvNetworkFactory = await ethers.getContractFactory('SSVNetwork');
-  const ssvNetwork = await upgrades.deployProxy(ssvNetworkFactory, [ssvRegistry.address, ssvToken.address, process.env.MINIMUM_BLOCKS_BEFORE_LIQUIDATION, process.env.OPERATOR_MAX_FEE_INCREASE, process.env.SET_OPERATOR_FEE_PERIOD, process.env.APPROVE_OPERATOR_FEE_PERIOD]);
+  const ssvNetwork = await upgrades.deployProxy(ssvNetworkFactory, [ssvRegistry.address, process.env.SSV_TOKEN_ADDRESS, process.env.MINIMUM_BLOCKS_BEFORE_LIQUIDATION, process.env.OPERATOR_MAX_FEE_INCREASE, process.env.SET_OPERATOR_FEE_PERIOD, process.env.APPROVE_OPERATOR_FEE_PERIOD]);
   await ssvNetwork.deployed();
-  console.log(`SSVToken: ${ssvToken.address}\nSSVRegistry: ${ssvRegistry.address}\nSSVNetwork: ${ssvNetwork.address}\n`);
+  console.log(`SSVToken: ${process.env.SSV_TOKEN_ADDRESS}\nSSVRegistry: ${ssvRegistry.address}\nSSVNetwork: ${ssvNetwork.address}\n`);
 }
 
 main()
