5f85d47e867cccd0fcf96a2792776d6bfa5ceb98
Vadim <vadim.inc@gmail.com>
2022-03-13 09:01:08 +0100

    migration flow

--- scripts/migration/ssv-network-migration.ts
+++ scripts/migration/ssv-network-migration.ts
@@ -3,6 +3,7 @@ import { ethers, upgrades } from 'hardhat';
 const _crypto = require('crypto');
 const Web3 = require('web3');
 const OLD_CONTRACT_ABI = require('./old_abi.json');
+const NEW_CONTRACT_ABI = require('./new_abi.json');
 
 function toChunks(items, size) {
   return Array.from(
@@ -24,6 +25,8 @@ function convertPublickey(rawValue) {
 async function main() {
   const web3 = new Web3(process.env.MIGRATION_NODE_URL);
   const oldContract = new web3.eth.Contract(OLD_CONTRACT_ABI, process.env.MIGRATION_OLD_CONTRACT_ADDRESS);
+  const newContract = new web3.eth.Contract(NEW_CONTRACT_ABI, process.env.MIGRATION_NEW_CONTRACT_ADDRESS);
+
   const ssvNetworkFactory = await ethers.getContractFactory('SSVNetwork');
   const ssvNetwork = await ssvNetworkFactory.attach(process.env.MIGRATION_NEW_CONTRACT_ADDRESS);
   const latestBlock = await web3.eth.getBlockNumber();
@@ -32,56 +35,41 @@ async function main() {
     toBlock: latestBlock
   };
 
+  /*
   console.log(`fetching operators...`, filters);
   const operatorEvents = await oldContract.getPastEvents('OperatorAdded', filters);
   console.log("total operatorEvents", operatorEvents.length);
-  let total = 0;
-  let params = [[],[],[],[]];
-  for (let index = 3; index < operatorEvents.length; index++) {
+  for (let index = 0; index < operatorEvents.length; index++) {
     const { returnValues } = operatorEvents[index];
-    if (total === 3) {
-      const tx = await ssvNetwork.batchRegisterOperator(
-        params[0],
-        params[1],
-        params[2],
-        params[3]
-      );
-      await tx.wait();
-      params[0].forEach((value, idx) => console.log('+', params[0][idx], params[1][idx], params[2][idx], params[3][idx]));
-      total = 0;
-      params = [[],[],[],[]];
-    }
-    params[0].push(returnValues.name);
-    params[1].push(returnValues.ownerAddress);
-    params[2].push(returnValues.publicKey);
-    params[3].push(0);
-    total++;
+    console.log('+', returnValues.name, returnValues.ownerAddress, returnValues.ownerAddress, returnValues.publicKey);
+    const tx = await ssvNetwork.migRegisterOperator(
+      returnValues.name,
+      returnValues.ownerAddress,
+      returnValues.publicKey,
+      0
+    );
+    await tx.wait();
   }
-  if (total > 0) {
-    try {
-      const tx = await ssvNetwork.batchRegisterOperator(
-        params[0],
-        params[1],
-        params[2],
-        params[3]
-      );
-      await tx.wait();
-      params[0].forEach((value, idx) => console.log('+', params[0][idx], params[1][idx], params[2][idx], params[3][idx]));  
-    } catch (e) {
-      console.log('------', params[0], e.message);
-    }
+  */
+  const newOperatorEvents = await newContract.getPastEvents('OperatorAdded', filters);
+  console.log("total new operatorEvents", newOperatorEvents.length);
+  const operatorIds = {};
+  for (let index = 0; index < newOperatorEvents.length; index++) {
+    const { returnValues } = newOperatorEvents[index];
+    operatorIds[returnValues.publicKey] = returnValues.id;
   }
-  /*
+
   console.log(`fetching validators...`, filters);
   const validatorEvents = await oldContract.getPastEvents('ValidatorAdded', filters);
   console.log("total validatorEvents", validatorEvents.length);
   for (let index = 0; index < validatorEvents.length; index++) {
     const { returnValues } = validatorEvents[index];
+    const operatorPubKeys = returnValues.operatorPublicKeys.map(key => operatorIds[key]);
     try {
-      const tx = await ssvNetwork.batchRegisterValidator(
+      const tx = await ssvNetwork.migRegisterValidator(
         returnValues.ownerAddress,
         returnValues.publicKey,
-        returnValues.operatorPublicKeys,
+        operatorPubKeys,
         returnValues.sharesPublicKeys,
         returnValues.encryptedKeys,
         0
@@ -92,7 +80,6 @@ async function main() {
       console.log(`${index}/${validatorEvents.length}`, '------', returnValues.publicKey);
     }
   }
-  */
 }
 
 main()
