1ffe8e96cb355cffafd10cd642b3f564471f7b86
Vadim <vadim.inc@gmail.com>
2022-04-03 16:21:29 +0200

    downgrade contracts

--- contracts/SSVNetwork.sol
+++ contracts/SSVNetwork.sol
@@ -64,7 +64,7 @@ contract SSVNetwork is Initializable, OwnableUpgradeable, ISSVNetwork {
     uint256 private _approveOperatorFeePeriod;
     mapping(uint256 => FeeChangeRequest) private _feeChangeRequests;
 
-    uint256 constant MINIMAL_OPERATOR_FEE = 0; // 10000
+    uint256 constant MINIMAL_OPERATOR_FEE = 10000;
 
     function initialize(
         ISSVRegistry registryAddress_,
