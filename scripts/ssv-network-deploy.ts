6ac6f7f39ae5e0b7a340c684613fded203e43881
Vadim <vadim@Vadims-MacBook-Pro-2.local>
2022-06-20 08:37:37 -0400

    prod deployment

--- scripts/ssv-network-deploy.ts
+++ scripts/ssv-network-deploy.ts
@@ -8,7 +8,7 @@ async function main() {
   const ssvTokenAddress = process.env.SSVTOKEN_ADDRESS;
   const ssvNetworkFactory = await ethers.getContractFactory('SSVNetwork');
   console.log(`Deploying SSVNetwork with ssvRegistry ${ssvRegistryAddress} and ssvToken ${ssvTokenAddress}...`);
-  const contract = await upgrades.deployProxy(ssvNetworkFactory, [ssvRegistryAddress, ssvTokenAddress, process.env.MINIMUM_BLOCKS_BEFORE_LIQUIDATION, process.env.OPERATOR_MAX_FEE_INCREASE, process.env.SET_OPERATOR_FEE_PERIOD, process.env.APPROVE_OPERATOR_FEE_PERIOD, process.env.VALIDATORS_PER_OPERATOR_LIMIT]);
+  const contract = await upgrades.deployProxy(ssvNetworkFactory, [ssvRegistryAddress, ssvTokenAddress, process.env.MINIMUM_BLOCKS_BEFORE_LIQUIDATION, process.env.OPERATOR_MAX_FEE_INCREASE, process.env.SET_OPERATOR_FEE_PERIOD, process.env.APPROVE_OPERATOR_FEE_PERIOD, process.env.VALIDATORS_PER_OPERATOR_LIMIT, process.env.OPERATORS_PER_OWNER_LIMIT]);
   await contract.deployed();
   console.log(`SSVNetwork deployed to: ${contract.address}`);
 }
