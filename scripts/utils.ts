de75abaa30b06fd07dd73345e7974f5b66248cd2
Vadim <vadim@Vadims-MacBook-Pro-2.local>
2022-07-20 21:44:07 +0200

    update deploy scripts by tags

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,13 +1,61 @@
 const fs = require('fs');
-const hre = require('hardhat');
+const simpleGit = require("simple-git");
 const ghpages = require('gh-pages');
+const hre = require('hardhat');
+const prompts = require('prompts');
+
+const git = simpleGit.default();
+
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 export async function publishAbi(name: string) {
+  const { latest, all } = await git.tags();
+  const gitTagChoices = [
+    { title: '[create new tag]', description: 'Create new git tag before contract deployment', value: 'create-tag' },
+    ...all.map((tag: any) => ({ title: tag, value: tag }))
+  ];
+  const response = await prompts([
+    {
+      type: 'select',
+      name: 'gitTag',
+      message: 'Git tag to deploy?',
+      choices: gitTagChoices
+    },
+    {
+      type: (prev: any) => prev === 'create-tag' ? 'text' : null,
+      name: 'newTag',
+      message: `Tag name?`,
+      validate: (value: any) => value === '' ? `Sorry, git tag can't be null` : true
+    },
+    {
+      type: (prev: any, values: any) => values.gitTag === 'create-tag' ? 'toggle' : null,
+      name: 'createTag',
+      initial: true,
+      active: 'yes',
+      inactive: 'no',
+      message: 'Create and publish new tag?'
+    }
+  ]);
+
+  let tag = null;
+  if (response.createTag) {
+    await git.addAnnotatedTag(response.newTag, `new version ${response.newTag}`);
+    await git.push('origin', response.newTag);
+    tag = response.newTag;
+  } else if (response.gitTag !== 'create-tag') {
+    tag = response.gitTag;
+  }
+
+  if (!tag) throw 'Please select git tag to deploy the contract.';
+
   const fullNames = await hre.artifacts.getAllFullyQualifiedNames();
   const contract = fullNames.find((item: string) => item.includes(`/${name}.sol`));
+
   const { abi, contractName } = await hre.artifacts.readArtifact(contract);
   const dir = `${hre.config.paths.root}/dist`;
-  await fs.promises.mkdir(`${dir}/abi`, { recursive: true });
-  await fs.promises.writeFile(`${dir}/abi/${contractName.toLowerCase()}.json`, `${JSON.stringify(abi, null, 2)}\n`, { flag: 'w' });
+  await fs.promises.mkdir(`${dir}/abi/${tag}`, { recursive: true });
+  await fs.promises.writeFile(`${dir}/abi/${tag}/${contractName.toLowerCase()}.json`, `${JSON.stringify(abi, null, 2)}\n`, { flag: 'w' });
   await ghpages.publish(dir);
+  console.log(`> ${name} contract abi published to:`, `https://bloxapp.github.io/ssv-network/abi/${tag}/${contractName.toLowerCase()}.json`);
 }
