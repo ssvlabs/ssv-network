cdc607f7ee5194e08727dab42887d4abb072b548
Vadim <vadim@Vadims-MacBook-Pro-2.local>
2022-08-08 12:11:35 +0200

    turn off optimizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -17,10 +17,12 @@ const config: HardhatUserConfig = {
       {
         version: '0.8.13',
         settings: {
+          /*
           optimizer: {
             enabled: true,
             runs: 10000
           }
+          */
         }
       }
     ],
