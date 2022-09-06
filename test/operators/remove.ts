0c4d4a79cbf3ddec4d7490a3b2a1bc5f2dc7d08d
Vadim <vadim@Vadims-MacBook-Pro-2.local>
2022-09-06 09:55:00 +0200

    remove operator tests

--- test/operators/remove.ts
+++ test/operators/remove.ts
@@ -1,28 +1,30 @@
 import * as helpers from '../helpers/contract-helpers';
 
 import { expect } from 'chai';
+import { trackGas, GasGroup } from '../helpers/gas-usage';
 
-const numberOfOperators = 4;
-const operatorFee = 4;
-
-let ssvNetworkContract: any, operatorIDs: any, shares: any, owner: any;
+let ssvNetworkContract: any;
 
 describe('Remove Operator Tests', () => {
   beforeEach(async () => {
-    const contractData = await helpers.initializeContract();
-    ssvNetworkContract = contractData.contract;
+    ssvNetworkContract = (await helpers.initializeContract()).contract;
   });
 
-  it('Remove operator', async () => {
-
+  it('Remove operator emits OperatorRemoved event', async () => {
+    await helpers.registerOperators(0, 1, '10');
+    await expect(ssvNetworkContract.connect(helpers.DB.owners[0]).removeOperator(1))
+      .to.emit(ssvNetworkContract, 'OperatorRemoved').withArgs(1);
   });
 
-  it('Remove operator errors', async () => {
-
+  it('Fails to remove operator with no owner', async () => {
+    await helpers.registerOperators(0, 1, '10');
+    await expect(ssvNetworkContract.connect(helpers.DB.owners[1]).removeOperator(1))
+      .to.be.revertedWith('CallerNotOwner');
   });
 
   it('Remove operator gas limits', async () => {
-
+    await helpers.registerOperators(0, 1, '10');
+    await trackGas(ssvNetworkContract.removeOperator(1), [GasGroup.REMOVE_OPERATOR]);
   });
 
 });
