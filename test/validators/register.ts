dd635676598acfe4b4d03f91aea48ef4dadc6c8a
AndrewBlox <andrew@blox.io>
2022-09-11 13:18:38 +0300

    Add validator tests

--- test/validators/register.ts
+++ test/validators/register.ts
@@ -1,53 +1,119 @@
-declare const ethers: any;
-
 import * as helpers from '../helpers/contract-helpers';
 
 import { expect } from 'chai';
-import { trackGas, GasGroup } from '../helpers/gas-usage';
+import { GasGroup } from '../helpers/gas-usage';
 
 let ssvNetworkContract: any;
 
 describe('Register Validator Tests', () => {
-  beforeEach(async () => {
-    ssvNetworkContract = (await helpers.initializeContract()).contract;
-    await helpers.registerOperators(0, 1, '10');
-    await helpers.registerOperators(1, 1, '10');
-    await helpers.registerOperators(2, 1, '10');
-    await helpers.registerOperators(3, 1, '10');
+    beforeEach(async () => {
+        // Initialize contract
+        ssvNetworkContract = (await helpers.initializeContract()).contract;
 
-    await helpers.deposit([0], ['100000']);
-    await helpers.deposit([1], ['100000']);
-  });
+        // Register operators
+        await helpers.registerOperators(0, 1, '10');
+        await helpers.registerOperators(1, 1, '10');
+        await helpers.registerOperators(2, 1, '10');
+        await helpers.registerOperators(3, 5, '10');
 
-  it('Register validator emits ValidatorAdded event', async () => {
-    await expect(ssvNetworkContract.registerValidator(
-      helpers.DataGenerator.publicKey(0),
-      helpers.DataGenerator.pod.new(),
-      helpers.DataGenerator.shares(0),
-      '10000'
-    )).to.emit(ssvNetworkContract, 'ValidatorAdded');
-  });
+        // Deposit into accounts
+        await helpers.deposit([0], ['100000']);
+        await helpers.deposit([1], ['100000']);
+    });
 
-  it('Register one validator in empty pod with gas track', async () => {
-    await helpers.registerValidators(0, 1, '10000', helpers.DataGenerator.pod.new(), [GasGroup.REGISTER_VALIDATOR_NEW_STATE]);
-  });
+    it('Register validator emits ValidatorAdded event', async () => {
+        await expect(ssvNetworkContract.registerValidator(
+            helpers.DataGenerator.publicKey(0),
+            helpers.DataGenerator.pod.new(),
+            helpers.DataGenerator.shares(0),
+            '10000'
+        )).to.emit(ssvNetworkContract, 'ValidatorAdded');
+    });
 
-  it('Register two validators in existed pod with gas track', async () => {
-    const result = await helpers.registerValidators(0, 1, '10000', helpers.DataGenerator.pod.new());
-    await helpers.registerValidators(0, 1, '10000', helpers.DataGenerator.pod.byId(result.podId), [GasGroup.REGISTER_VALIDATOR_EXISTED_POD]);
-  });
+    it('Register one validator into an empty pod with gas tracking', async () => {
+        await helpers.registerValidators(0, 1, '10000', helpers.DataGenerator.pod.new(), [GasGroup.REGISTER_VALIDATOR_NEW_STATE]);
+    });
 
-  it('Register two validators in existed cluster with gas track', async () => {
-    const result = await helpers.registerValidators(0, 1, '10000', helpers.DataGenerator.pod.new());
-    await helpers.registerValidators(1, 1, '10000', helpers.DataGenerator.pod.byId(result.podId), [GasGroup.REGISTER_VALIDATOR_EXISTED_CLUSTER]);
-  });
+    it('Register two validators into an existing pod with gas tracking', async () => {
+        const result = await helpers.registerValidators(0, 1, '10000', helpers.DataGenerator.pod.new());
+        await helpers.registerValidators(0, 1, '10000', helpers.DataGenerator.pod.byId(result.podId), [GasGroup.REGISTER_VALIDATOR_EXISTED_POD]);
+    });
 
-  it('Fails to register with invalid operator list size', async () => {
-    await expect(ssvNetworkContract.registerValidator(
-      helpers.DataGenerator.publicKey(0),
-      [1, 2],
-      helpers.DataGenerator.shares(0),
-      '10000'
-    )).to.be.revertedWith('OessDataStructureInvalid');
-  });
+    it('Register two validators into an existing cluster with gas tracking', async () => {
+        const result = await helpers.registerValidators(0, 1, '10000', helpers.DataGenerator.pod.new());
+        await helpers.registerValidators(1, 1, '10000', helpers.DataGenerator.pod.byId(result.podId), [GasGroup.REGISTER_VALIDATOR_EXISTED_CLUSTER]);
+    });
+
+    it('Register with 7 operators', async () => {
+        await expect(ssvNetworkContract.registerValidator(
+            helpers.DataGenerator.publicKey(0),
+            [1, 2, 3, 4, 5, 6, 7],
+            helpers.DataGenerator.shares(0),
+            '4000'
+        )).to.emit(ssvNetworkContract, 'ValidatorAdded');
+    });
+
+    it('Invalid operator amount', async () => {
+        // 2 Operators
+        await expect(ssvNetworkContract.registerValidator(
+            helpers.DataGenerator.publicKey(0),
+            [1, 2],
+            helpers.DataGenerator.shares(0),
+            '10000'
+        )).to.be.revertedWith('OessDataStructureInvalid');
+
+        // 6 Operators
+        await expect(ssvNetworkContract.registerValidator(
+            helpers.DataGenerator.publicKey(0),
+            [1, 2, 3, 4, 5, 6],
+            helpers.DataGenerator.shares(0),
+            '10000'
+        )).to.be.revertedWith('OessDataStructureInvalid');
+    });
+
+    it('Invalid public key length', async () => {
+        await expect(ssvNetworkContract.registerValidator(
+            helpers.DataGenerator.shares(0),
+            [1, 2, 3, 4],
+            helpers.DataGenerator.shares(0),
+            '10000'
+        )).to.be.revertedWith('InvalidPublicKeyLength');
+    });
+
+    it('Not enough amount', async () => {
+        await expect(ssvNetworkContract.registerValidator(
+            helpers.DataGenerator.publicKey(0),
+            [1, 2, 3, 4],
+            helpers.DataGenerator.shares(0),
+            '1'
+        )).to.be.revertedWith('account liquidatable');
+    });
+
+    // UPDATE ONCE LOGIC IS IMPLEMENTED
+    // it('Invalid share', async () => {
+    //     await expect(ssvNetworkContract.registerValidator(
+    //         helpers.DataGenerator.publicKey(0),
+    //         [1, 2, 3, 4],
+    //         helpers.DataGenerator.publicKey(0),
+    //         '10000'
+    //     )).to.be.revertedWith('InvalidShareLength');
+    // });
+
+    // it('Not enough deposited', async () => {
+    //     await expect(ssvNetworkContract.registerValidator(
+    //         helpers.DataGenerator.publicKey(0),
+    //         [1, 2, 3, 4],
+    //         helpers.DataGenerator.shares(0),
+    //         '100001'
+    //     )).to.be.revertedWith('NotEnoungBalance');
+    // });
+
+    // it('Non existent operator', async () => {
+    //     await expect(ssvNetworkContract.registerValidator(
+    //         helpers.DataGenerator.publicKey(0),
+    //         [1, 2, 3, 25],
+    //         helpers.DataGenerator.shares(0),
+    //         '4000'
+    //     )).to.be.revertedWith('OperatorDoesntExist');
+    // });
 });
