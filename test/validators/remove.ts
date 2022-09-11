2b19e5834caaea9a01839900d605139edf79b2e9
AndrewBlox <andrew@blox.io>
2022-09-11 13:13:06 +0300

    Update validator tests

--- test/validators/remove.ts
+++ test/validators/remove.ts
@@ -1,39 +1,63 @@
-declare const ethers: any;
-
 import * as helpers from '../helpers/contract-helpers';
 
 import { expect } from 'chai';
 import { trackGas, GasGroup } from '../helpers/gas-usage';
 
-
 let ssvNetworkContract: any;
 
 describe('Remove Validator Tests', () => {
-  beforeEach(async () => {
-    ssvNetworkContract = (await helpers.initializeContract()).contract;
-    await helpers.registerOperators(0, 1, '10');
-    await helpers.registerOperators(1, 1, '10');
-    await helpers.registerOperators(2, 1, '10');
-    await helpers.registerOperators(3, 1, '10');
+    beforeEach(async () => {
+        // Initialize contract
+        ssvNetworkContract = (await helpers.initializeContract()).contract;
 
-    await helpers.deposit([4], ['100000']);
-  });
+        // Register operators
+        await helpers.registerOperators(0, 1, '10');
+        await helpers.registerOperators(1, 1, '10');
+        await helpers.registerOperators(2, 1, '10');
+        await helpers.registerOperators(3, 1, '10');
 
-  it('Remove validator emits ValidatorRemoved event', async () => {
-    const { validators } = await helpers.registerValidators(4, 1, '10000', helpers.DataGenerator.pod.new());
+        // Deposit into accounts
+        await helpers.deposit([4], ['100000']);
+    });
 
-    await expect(ssvNetworkContract.connect(helpers.DB.owners[4]).removeValidator(
-      validators[0].publicKey,
-    )).to.emit(ssvNetworkContract, 'ValidatorRemoved');
-  });
+    it('Remove validator emits ValidatorRemoved event', async () => {
+        // Register validator
+        const { validators } = await helpers.registerValidators(4, 1, '10000', helpers.DataGenerator.pod.new());
 
-  it('Remove validator track gas', async () => {
-    const { validators } = await helpers.registerValidators(4, 1, '10000', helpers.DataGenerator.pod.new());
-    await trackGas(ssvNetworkContract.connect(helpers.DB.owners[4]).removeValidator(validators[0].publicKey), [GasGroup.REMOVE_VALIDATOR]);
-  });
+        // Remove validator
+        await expect(ssvNetworkContract.connect(helpers.DB.owners[4]).removeValidator(
+            validators[0].publicKey,
+        )).to.emit(ssvNetworkContract, 'ValidatorRemoved');
+    });
 
-  it('Fails to remove validator with no owner', async () => {
-    const { validators } = await helpers.registerValidators(4, 1, '10000', helpers.DataGenerator.pod.new());
-    await expect(ssvNetworkContract.connect(helpers.DB.owners[3]).removeValidator(validators[0].publicKey)).to.be.revertedWith('ValidatorNotOwned');
-  });
+    it('Remove validator with gas tracking', async () => {
+        // Register validator
+        const { validators } = await helpers.registerValidators(4, 1, '10000', helpers.DataGenerator.pod.new());
+
+        // Remove validator
+        await trackGas(ssvNetworkContract.connect(helpers.DB.owners[4]).removeValidator(validators[0].publicKey), [GasGroup.REMOVE_VALIDATOR]);
+    });
+
+    it('Fails to remove validator with an invalid owner', async () => {
+        // Register validator
+        const { validators } = await helpers.registerValidators(4, 1, '10000', helpers.DataGenerator.pod.new());
+
+        // Remove validator
+        await expect(ssvNetworkContract.connect(helpers.DB.owners[3]).removeValidator(validators[0].publicKey)).to.be.revertedWith('ValidatorNotOwned');
+    });
+
+    // UPDATE ONCE LIQUIDATE FUNCTION IS IMPLEMENTED
+    // it('Remove validator from a liquidated pod', async () => {
+    //     // Register validator
+    //     const { validators } = await helpers.registerValidators(4, 1, '10000', helpers.DataGenerator.pod.new());
+
+    //     // Liquidate pod
+    //     // Progress blocks to liquidatable state
+    //     // Liquidate pod
+
+    //     // Remove validator
+    //     await expect(ssvNetworkContract.connect(helpers.DB.owners[4]).removeValidator(
+    //         validators[0].publicKey,
+    //     )).to.emit(ssvNetworkContract, 'ValidatorRemoved');
+    // });
 });
--- test/validators/transfer.ts
+++ test/validators/transfer.ts
@@ -1,5 +1,3 @@
-declare const ethers: any;
-
 import * as helpers from '../helpers/contract-helpers';
 
 import { expect } from 'chai';
@@ -9,7 +7,10 @@ let ssvNetworkContract: any;
 
 describe('Transfer Validator Tests', () => {
   beforeEach(async () => {
+    // Initialize contract
     ssvNetworkContract = (await helpers.initializeContract()).contract;
+
+    // Register operators
     await helpers.registerOperators(0, 1, '10');
     await helpers.registerOperators(1, 1, '10');
     await helpers.registerOperators(2, 1, '10');
@@ -19,13 +20,16 @@ describe('Transfer Validator Tests', () => {
     await helpers.registerOperators(6, 1, '10');
     await helpers.registerOperators(7, 1, '10');
 
+    // Deposit into accounts
     await helpers.deposit([4], ['100000']);
     await helpers.deposit([5], ['100000']);
   });
 
   it('Transfer validator emits ValidatorTransferred event', async () => {
+    // Register validator
     const { validators } = await helpers.registerValidators(4, 1, '10000', helpers.DataGenerator.pod.new());
 
+    // Transfer validator to new pod
     await expect(ssvNetworkContract.connect(helpers.DB.owners[4]).transferValidator(
       validators[0].publicKey,
       helpers.DataGenerator.pod.new(),
@@ -34,9 +38,11 @@ describe('Transfer Validator Tests', () => {
     )).to.emit(ssvNetworkContract, 'ValidatorTransferred');
   });
 
-  it('Transfer validator into new pod track gas', async () => {
+  it('Transfer validator into a new pod with gas tracking', async () => {
+    // Register validator
     const { validators, podId } = await helpers.registerValidators(4, 1, '10000', helpers.DataGenerator.pod.new());
 
+    // Transfer validator to new pod
     const transferedValidator = await trackGas(ssvNetworkContract.connect(helpers.DB.owners[4]).transferValidator(
       validators[0].publicKey,
       helpers.DataGenerator.pod.new(),
@@ -47,9 +53,12 @@ describe('Transfer Validator Tests', () => {
     expect(podId).not.equals(transferedValidator.eventsByName.ValidatorTransferred[0].args.podId);
   });
 
-  it('Transfer validator to existed pod track gas', async () => {
+  it('Transfer validator to an existing pod with gas tracking', async () => {
+    // Register validators
     const validator1 = await helpers.registerValidators(4, 1, '10000', helpers.DataGenerator.pod.new());
     const { podId } = await helpers.registerValidators(4, 1, '10000', helpers.DataGenerator.pod.new());
+
+    // Transfer validator to an existing pod
     const transfredValidator1 = await trackGas(ssvNetworkContract.connect(helpers.DB.owners[4]).transferValidator(
       validator1.validators[0].publicKey,
       helpers.DataGenerator.pod.byId(podId),
@@ -60,9 +69,12 @@ describe('Transfer Validator Tests', () => {
     expect(podId).equals(transfredValidator1.eventsByName.ValidatorTransferred[0].args.podId);
   });
 
-  it('Transfer validator to existed cluster track gas', async () => {
+  it('Transfer validator to an existing cluster with gas tracking', async () => {
+    // Register validators
     const validator1 = await helpers.registerValidators(4, 1, '10000', helpers.DataGenerator.pod.new());
     const { podId } = await helpers.registerValidators(5, 1, '10000', helpers.DataGenerator.pod.new());
+
+    // Transfer validator to an existing pod
     const transfredValidator1 = await trackGas(ssvNetworkContract.connect(helpers.DB.owners[4]).transferValidator(
       validator1.validators[0].publicKey,
       helpers.DataGenerator.pod.byId(podId),
@@ -73,14 +85,89 @@ describe('Transfer Validator Tests', () => {
     expect(podId).equals(transfredValidator1.eventsByName.ValidatorTransferred[0].args.podId);
   });
 
-  it('Fails to transfer validator with no owner', async () => {
+  it('Transfer validator with an invalid owner', async () => {
+    // Register validators
     const validator1 = await helpers.registerValidators(4, 1, '10000', helpers.DataGenerator.pod.new());
     const { podId } = await helpers.registerValidators(4, 1, '10000', helpers.DataGenerator.pod.new());
+
+    // Transfer validator with an invalid owner
     await expect(ssvNetworkContract.connect(helpers.DB.owners[5]).transferValidator(
       validator1.validators[0].publicKey,
       helpers.DataGenerator.pod.byId(podId),
       helpers.DataGenerator.shares(helpers.DB.validators.length),
       '10000'
     )).to.be.revertedWith('ValidatorNotOwned');
+
+    // Transfer validator with an invalid public key
+    await expect(ssvNetworkContract.connect(helpers.DB.owners[4]).transferValidator(
+      helpers.DataGenerator.shares(0),
+      helpers.DataGenerator.pod.byId(podId),
+      helpers.DataGenerator.shares(helpers.DB.validators.length),
+      '10000'
+    )).to.be.revertedWith('ValidatorNotOwned');
   });
+
+  it('Transfer validator to pod with 7 operators', async () => {
+    // Register validators
+    const { validators } = await helpers.registerValidators(4, 1, '10000', helpers.DataGenerator.pod.new());
+    const { podId } = await helpers.registerValidators(4, 1, '10000', [1, 2, 3, 4, 5, 6, 7]);
+
+    // Transfer validator to an existing pod
+    await expect(ssvNetworkContract.connect(helpers.DB.owners[4]).transferValidator(
+      validators[0].publicKey,
+      helpers.DataGenerator.pod.byId(podId),
+      helpers.DataGenerator.shares(helpers.DB.validators.length),
+      '10000'
+    )).to.emit(ssvNetworkContract, 'ValidatorTransferred');
+  });
+
+  // UPDATE ONCE LOGIC IS IMPLEMENTED
+  // it('Transfer validator with not enough amount', async () => {
+  //   // Register validators
+  //   const validator1 = await helpers.registerValidators(4, 1, '10000', helpers.DataGenerator.pod.new());
+  //   const { podId } = await helpers.registerValidators(4, 1, '10000', [1, 2, 3, 9]);
+
+  //   // Increase operator fee
+  //   await ssvNetworkContract.connect(helpers.DB.owners[8]).updateOperatorFee(9, '100000')
+
+  //   // Transfer to pod with not enough amount
+  //   await expect(ssvNetworkContract.connect(helpers.DB.owners[4]).transferValidator(
+  //     validator1.validators[0].publicKey,
+  //     helpers.DataGenerator.pod.byId(podId),
+  //     helpers.DataGenerator.shares(helpers.DB.validators.length),
+  //     '1'
+  //   )).to.be.revertedWith('account liquidatable');
+  // });
+
+  // it('Transfer validator with an invalid pod', async () => {
+  //   const validator1 = await helpers.registerValidators(4, 1, '10000', helpers.DataGenerator.pod.new());
+  //   await expect(ssvNetworkContract.connect(helpers.DB.owners[4]).transferValidator(
+  //     validator1.validators[0].publicKey,
+  //     validator1.validators[0].publicKey,
+  //     helpers.DataGenerator.shares(helpers.DB.validators.length),
+  //     '10000'
+  //   )).to.be.revertedWith('InvalidPod');
+  // });
+
+  // it('Transfer validator with not enough balance', async () => {
+  //   const validator1 = await helpers.registerValidators(4, 1, '10000', helpers.DataGenerator.pod.new());
+  //   const { podId } = await helpers.registerValidators(4, 1, '10000', helpers.DataGenerator.pod.new());
+  //   await expect(ssvNetworkContract.connect(helpers.DB.owners[4]).transferValidator(
+  //     validator1.validators[0].publicKey,
+  //     helpers.DataGenerator.pod.byId(podId),
+  //     helpers.DataGenerator.shares(helpers.DB.validators.length),
+  //     '100001'
+  //   )).to.be.revertedWith('NotEnoughBalance');
+  // });
+
+  // it('Transfer validator with an invalid share', async () => {
+  //   await helpers.registerValidators(4, 1, '10000', helpers.DataGenerator.pod.new());
+  //   const { podId } = await helpers.registerValidators(4, 1, '10000', helpers.DataGenerator.pod.new());
+  //   await expect(ssvNetworkContract.connect(helpers.DB.owners[4]).transferValidator(
+  //     helpers.DataGenerator.publicKey(0),
+  //     helpers.DataGenerator.pod.byId(podId),
+  //     helpers.DataGenerator.shares(helpers.DB.validators.length),
+  //     '10000'
+  //   )).to.be.revertedWith('InvalidShares');
+  // });
 });
