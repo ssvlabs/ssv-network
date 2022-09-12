0df7284f49fc378353f990a50a000dba356a260f
Wadym <vadim.inc@gmail.com>
2022-09-12 11:27:08 +0200

    Revert "Tests operator remove--IO1-1741 (#93)"
    This reverts commit 698bad80abccaea2c50ff152525d41b315ceedfa.

--- test/operators/remove.ts
+++ test/operators/remove.ts
@@ -1,30 +1,28 @@
 import * as helpers from '../helpers/contract-helpers';
 
 import { expect } from 'chai';
-import { trackGas, GasGroup } from '../helpers/gas-usage';
 
-let ssvNetworkContract: any;
+const numberOfOperators = 4;
+const operatorFee = 4;
+
+let ssvNetworkContract: any, operatorIDs: any, shares: any, owner: any;
 
 describe('Remove Operator Tests', () => {
   beforeEach(async () => {
-    ssvNetworkContract = (await helpers.initializeContract()).contract;
+    const contractData = await helpers.initializeContract();
+    ssvNetworkContract = contractData.contract;
   });
 
-  it('Remove operator emits OperatorRemoved event', async () => {
-    await helpers.registerOperators(0, 1, '10');
-    await expect(ssvNetworkContract.connect(helpers.DB.owners[0]).removeOperator(1))
-      .to.emit(ssvNetworkContract, 'OperatorRemoved').withArgs(1);
+  it('Remove operator', async () => {
+
   });
 
-  it('Fails to remove operator with no owner', async () => {
-    await helpers.registerOperators(0, 1, '10');
-    await expect(ssvNetworkContract.connect(helpers.DB.owners[1]).removeOperator(1))
-      .to.be.revertedWith('CallerNotOwner');
+  it('Remove operator errors', async () => {
+
   });
 
   it('Remove operator gas limits', async () => {
-    await helpers.registerOperators(0, 1, '10');
-    await trackGas(ssvNetworkContract.removeOperator(1), [GasGroup.REMOVE_OPERATOR]);
+
   });
 
 });
