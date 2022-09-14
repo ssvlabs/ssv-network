5aa17200f7ba92a9158e9d6d46864f096565e19a
AndrewBlox <andrew@blox.io>
2022-09-14 12:48:13 +0300

    update remove testing

--- test/validators/remove.ts
+++ test/validators/remove.ts
@@ -16,33 +16,48 @@ describe('Remove Validator Tests', () => {
         // Deposit into accounts
         await helpers.deposit([4], ['100000']);
 
+        // Register a validator
         podResult = await helpers.registerValidators(4, 1, '10000', helpers.DataGenerator.pod.new(), [GasGroup.REGISTER_VALIDATOR_NEW_STATE]);
     });
 
     it('Remove validator emits ValidatorRemoved event', async () => {
-        // Remove validator
         await expect(ssvNetworkContract.connect(helpers.DB.owners[4]).removeValidator(
             podResult.validators[0].publicKey,
         )).to.emit(ssvNetworkContract, 'ValidatorRemoved');
     });
 
     it('Remove validator', async () => {
-        // Remove validator
         await trackGas(ssvNetworkContract.connect(helpers.DB.owners[4]).removeValidator(podResult.validators[0].publicKey), [GasGroup.REMOVE_VALIDATOR]);
     });
 
-    it('Fail to remove validator with an invalid owner', async () => {
-        // Remove validator
+    it('Remove validator with an invalid owner', async () => {
         await expect(ssvNetworkContract.connect(helpers.DB.owners[3]).removeValidator(podResult.validators[0].publicKey)).to.be.revertedWith('ValidatorNotOwned');
     });
 
     // THIS SHOULD PASS
-    it('Try to remove validator twice', async () => {
+    it('Remove validator twice', async () => {
         // Remove validator
         await trackGas(ssvNetworkContract.connect(helpers.DB.owners[4]).removeValidator(podResult.validators[0].publicKey), [GasGroup.REMOVE_VALIDATOR]);
 
         // Remove validator again
         await expect(ssvNetworkContract.connect(helpers.DB.owners[4]).removeValidator(podResult.validators[0].publicKey)).to.be.revertedWith('ValidatorNotOwned');
+    });
+
+    // THIS SHOULD PASS
+    it('Register / remove validator twice', async () => {
+        // Remove validator
+        await trackGas(ssvNetworkContract.connect(helpers.DB.owners[4]).removeValidator(podResult.validators[0].publicKey), [GasGroup.REMOVE_VALIDATOR]);
+
+        // Re-register validator
+        await ssvNetworkContract.connect(helpers.DB.owners[4]).registerValidator(
+            helpers.DataGenerator.publicKey(0),
+            [1, 2, 3, 4],
+            helpers.DataGenerator.shares(0),
+            '1000'
+        );
+
+        // Remove the validator again
+        await trackGas(ssvNetworkContract.connect(helpers.DB.owners[4]).removeValidator(helpers.DataGenerator.publicKey(0)), [GasGroup.REMOVE_VALIDATOR]);
     });
 
     // UPDATE ONCE LIQUIDATE FUNCTION IS IMPLEMENTED
