795510549977acdd8d56fc3e8ae3bf73caeeb79e
Dmitri Meshin <dmitri@blox.io>
2022-11-09 15:24:39 +0200

    SSVNetwork deploy script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -49,8 +49,8 @@ if (process.env.GOERLI_ETH_NODE_URL) {
   config.networks.goerli = {
     url: process.env.GOERLI_ETH_NODE_URL,
     accounts: [`0x${process.env.GOERLI_OWNER_PRIVATE_KEY}`],
-    gasPrice: +(process.env.GAS_PRICE || ''),
-    gas: +(process.env.GAS || '')
+    gasPrice: process.env.GAS_PRICE == "auto" ? "auto" : Number(process.env.GAS_PRICE),
+    gas: process.env.GAS == "auto" ? "auto" : Number(process.env.GAS)
   };
 }
 
