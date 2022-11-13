4fba3394d8c59bf512e0a21061801380b65e8cf4
AndrewBlox <andrew@blox.io>
2022-11-13 13:40:08 +0200

    Update deploy script

---
+++ scripts/ssv-network-deploy.ts
@@ -0,0 +1,22 @@
+import { ethers, upgrades } from 'hardhat';
+
+async function main() {
+  const ssvTokenAddress = process.env.SSVTOKEN_ADDRESS;
+  const ssvNetworkFactory = await ethers.getContractFactory('SSVNetwork');
+  console.log(`Deploying SSVNetwork with ssvToken ${ssvTokenAddress}...`);
+  const contract = await upgrades.deployProxy(ssvNetworkFactory, [
+    ssvTokenAddress,
+    process.env.OPERATOR_MAX_FEE_INCREASE,
+    process.env.DECLARE_OPERATOR_FEE_PERIOD,
+    process.env.EXECUTE_OPERATOR_FEE_PERIOD
+  ]);
+  await contract.deployed();
+  console.log(`SSVNetwork deployed to: ${contract.address}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
