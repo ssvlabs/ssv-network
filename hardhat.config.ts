25803bfdf6a768aaa26716a05e8bf85099fb0ac6
AndrewBlox <andrew@blox.io>
2022-11-13 13:55:51 +0200

    hardhat changes

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,9 +12,9 @@ import '@nomiclabs/hardhat-solhint';
 
 const config: HardhatUserConfig = {
   // Your type-safe config goes here
-  mocha: {
-    timeout: 40000000000000000
-  },
+  // mocha: {
+  //   timeout: 4000000000000
+  // },
   solidity: {
     compilers: [
       {
@@ -22,45 +22,53 @@ const config: HardhatUserConfig = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 10000
+            runs: 1000
           }
         }
       }
     ],
   },
   networks: {
-    hardhat: {
-      allowUnlimitedContractSize: true
+    // hardhat: {
+    //   allowUnlimitedContractSize: true
+    // },
+    goerli: {
+      allowUnlimitedContractSize: true,
+      url: process.env.GOERLI_ETH_NODE_URL,
+      accounts: [`0x${process.env.GOERLI_OWNER_PRIVATE_KEY}`],
+      gas: 'auto',
+      gasPrice: 'auto'
     }
   },
-  etherscan: {
-    // Your API key for Etherscan
-    // Obtain one at https://etherscan.io/
-    apiKey: process.env.ETHERSCAN_KEY
-  },
-  gasReporter: {
-    currency: 'USD',
-    gasPrice: 0.3
-  }
+  // etherscan: {
+  //   // Your API key for Etherscan
+  //   // Obtain one at https://etherscan.io/
+  //   apiKey: process.env.ETHERSCAN_KEY
+  // },
+  // gasReporter: {
+  //   currency: 'USD',
+  //   gasPrice: 0.3
+  // }
 };
 
-if (process.env.GOERLI_ETH_NODE_URL) {
-  //@ts-ignore
-  config.networks.goerli = {
-    url: process.env.GOERLI_ETH_NODE_URL,
-    accounts: [`0x${process.env.GOERLI_OWNER_PRIVATE_KEY}`],
-    gasPrice: +(process.env.GAS_PRICE || ''),
-    gas: +(process.env.GAS || '')
-  };
-}
+// if (process.env.GOERLI_ETH_NODE_URL) {
+//   //@ts-ignore
+//   config.networks.goerli = {
+//       allowUnlimitedContractSize: true,
+//       url: process.env.GOERLI_ETH_NODE_URL,
+//       accounts: [`0x${process.env.GOERLI_OWNER_PRIVATE_KEY}`],
+//       gasPrice: "auto",
+//       gas: "auto"
+//   };
+// }
 
-if (process.env.MAINNET_ETH_NODE_URL) {
-  //@ts-ignore
-  config.networks.mainnet = {
-    url: process.env.MAINNET_ETH_NODE_URL,
-    accounts: [`0x${process.env.MAINNET_OWNER_PRIVATE_KEY}`],
-    gasPrice: +(process.env.GAS_PRICE || '')
-  };
-}
+// if (process.env.MAINNET_ETH_NODE_URL) {
+//   //@ts-ignore
+//   config.networks.mainnet = {
+//     url: process.env.MAINNET_ETH_NODE_URL,
+//     accounts: [`0x${process.env.MAINNET_OWNER_PRIVATE_KEY}`],
+//     gasPrice: +(process.env.GAS_PRICE || '')
+//   };
+// }
 
 export default config;
