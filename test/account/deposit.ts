db119e6d32aaa6904cc632d33e352b0efa1eb919
Or-blox <117833150+Or-blox@users.noreply.github.com>
2022-12-20 17:59:51 +0200

    Or deposit tests  (#135)
    * Changed deposit tests order
    
    * deposit tests

--- test/account/deposit.ts
+++ test/account/deposit.ts
@@ -3,6 +3,7 @@ import * as helpers from '../helpers/contract-helpers';
 import { expect } from 'chai';
 import { trackGas, GasGroup } from '../helpers/gas-usage';
 
+// Declare globals
 let ssvNetworkContract: any, pod1: any, minDepositAmount: any;
 
 describe('Deposit Tests', () => {
@@ -41,9 +42,19 @@ describe('Deposit Tests', () => {
     await expect(ssvNetworkContract.connect(helpers.DB.owners[4])['deposit(uint64[],uint256,(uint32,uint64,uint64,uint64,uint64,bool))'](pod1.args.operatorIds, minDepositAmount, pod1.args.pod)).to.emit(ssvNetworkContract, 'FundsDeposit');
   });
 
+  it('Deposit to a pod I own gas limits', async () => {
+    await helpers.DB.ssvToken.connect(helpers.DB.owners[4]).approve(ssvNetworkContract.address, minDepositAmount);
+    await trackGas(ssvNetworkContract.connect(helpers.DB.owners[4])['deposit(uint64[],uint256,(uint32,uint64,uint64,uint64,uint64,bool))'](pod1.args.operatorIds, minDepositAmount, pod1.args.pod), [GasGroup.DEPOSIT]);
+  });
+
   it('Deposit to a pod I do not own emits "FundsDeposit"', async () => {
     await helpers.DB.ssvToken.connect(helpers.DB.owners[0]).approve(ssvNetworkContract.address, minDepositAmount);
     await expect(ssvNetworkContract.connect(helpers.DB.owners[0])['deposit(address,uint64[],uint256,(uint32,uint64,uint64,uint64,uint64,bool))'](helpers.DB.owners[4].address, pod1.args.operatorIds, minDepositAmount, pod1.args.pod)).to.emit(ssvNetworkContract, 'FundsDeposit');
+  });
+
+  it('Deposit to a pod I do not own gas limits', async () => {
+    await helpers.DB.ssvToken.connect(helpers.DB.owners[0]).approve(ssvNetworkContract.address, minDepositAmount);
+    await trackGas(ssvNetworkContract.connect(helpers.DB.owners[0])['deposit(address,uint64[],uint256,(uint32,uint64,uint64,uint64,uint64,bool))'](helpers.DB.owners[4].address, pod1.args.operatorIds, minDepositAmount, pod1.args.pod), [GasGroup.DEPOSIT]);
   });
 
   it('Deposit to a pod I do own with a pod that does not exist reverts "PodNotExists"', async () => {
@@ -52,15 +63,5 @@ describe('Deposit Tests', () => {
 
   it('Deposit to a pod I do not own with a pod that does not exist reverts "PodNotExists"', async () => {
     await expect(ssvNetworkContract.connect(helpers.DB.owners[4])['deposit(uint64[],uint256,(uint32,uint64,uint64,uint64,uint64,bool))']([1,2,4,5], minDepositAmount, pod1.args.pod)).to.be.revertedWith('PodNotExists');
-  });
-
-  it('Deposit to a pod I own gas limits', async () => {
-    await helpers.DB.ssvToken.connect(helpers.DB.owners[4]).approve(ssvNetworkContract.address, minDepositAmount);
-    await trackGas(ssvNetworkContract.connect(helpers.DB.owners[4])['deposit(uint64[],uint256,(uint32,uint64,uint64,uint64,uint64,bool))'](pod1.args.operatorIds, minDepositAmount, pod1.args.pod), [GasGroup.DEPOSIT]);
-  });
-
-  it('Deposit to a pod I do not own gas limits', async () => {
-    await helpers.DB.ssvToken.connect(helpers.DB.owners[0]).approve(ssvNetworkContract.address, minDepositAmount);
-    await trackGas(ssvNetworkContract.connect(helpers.DB.owners[0])['deposit(address,uint64[],uint256,(uint32,uint64,uint64,uint64,uint64,bool))'](helpers.DB.owners[4].address, pod1.args.operatorIds, minDepositAmount, pod1.args.pod), [GasGroup.DEPOSIT]);
   });
 });
