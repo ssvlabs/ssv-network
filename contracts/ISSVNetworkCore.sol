63b47b6a1941acbad6947840cd6a705366dd393a
Marco <matabasco@gmail.com>
2023-05-30 13:52:39 +0200

    restore cluster order

--- contracts/ISSVNetworkCore.sol
+++ contracts/ISSVNetworkCore.sol
@@ -43,8 +43,8 @@ interface ISSVNetworkCore {
         uint32 validatorCount;
         uint64 networkFeeIndex;
         uint64 index;
-        uint256 balance;
         bool active;
+        uint256 balance;
     }
 
     struct DAO {
