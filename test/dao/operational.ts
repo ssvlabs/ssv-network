be4f1560c7a46eddad82e3e5eed029204fc9e09f
Marco <matabasco@gmail.com>
2023-10-30 16:08:47 +0100

    add liquidation tets

--- test/dao/operational.ts
+++ test/dao/operational.ts
@@ -1,9 +1,11 @@
 // Declare imports
 import * as helpers from '../helpers/contract-helpers';
 import { trackGas } from '../helpers/gas-usage';
+import { progressBlocks } from '../helpers/utils';
+
 import { expect } from 'chai';
 
-let ssvNetworkContract: any, ssvNetworkViews: any;
+let ssvNetworkContract: any, ssvNetworkViews: any, firstCluster: any;
 
 // Declare globals
 describe('DAO operational Tests', () => {
@@ -27,12 +29,13 @@ describe('DAO operational Tests', () => {
         expect(await ssvNetworkContract.owner()).equals(helpers.DB.owners[4].address);
     });
 
-    it('Get the network validators count when updating it', async () => {
+    it('Get the network validators count (add/remove validaotor)', async () => {
         await helpers.registerOperators(0, 4, helpers.CONFIG.minimalOperatorFee);
 
         const deposit = (helpers.CONFIG.minimalBlocksBeforeLiquidation + 2) * helpers.CONFIG.minimalOperatorFee * 13;
 
         await helpers.DB.ssvToken.connect(helpers.DB.owners[4]).approve(ssvNetworkContract.address, deposit);
+
         const register = await trackGas(ssvNetworkContract.connect(helpers.DB.owners[4]).registerValidator(
             helpers.DataGenerator.publicKey(1),
             [1, 2, 3, 4],
@@ -46,15 +49,36 @@ describe('DAO operational Tests', () => {
                 active: true
             }
         ));
-        const cluster1 = register.eventsByName.ValidatorAdded[0].args;
+
+        firstCluster = register.eventsByName.ValidatorAdded[0].args;
 
         expect((await ssvNetworkViews.getNetworkValidatorsCount())).to.equal(1);
 
         await ssvNetworkContract.connect(helpers.DB.owners[4]).removeValidator(
             helpers.DataGenerator.publicKey(1),
-            cluster1.operatorIds,
-            cluster1.cluster
+            firstCluster.operatorIds,
+            firstCluster.cluster
         );
         expect((await ssvNetworkViews.getNetworkValidatorsCount())).to.equal(0);
     });
+
+    it('Get the network validators count (add/remove validaotor)', async () => {
+        await helpers.registerOperators(0, 4, helpers.CONFIG.minimalOperatorFee);
+
+        const deposit = (helpers.CONFIG.minimalBlocksBeforeLiquidation + 2) * helpers.CONFIG.minimalOperatorFee * 13;
+        firstCluster = await helpers.registerValidators(4, 1, deposit.toString(), helpers.DataGenerator.cluster.new());
+       
+        expect((await ssvNetworkViews.getNetworkValidatorsCount())).to.equal(1);
+        
+        await progressBlocks(helpers.CONFIG.minimalBlocksBeforeLiquidation);
+
+        await ssvNetworkContract.connect(helpers.DB.owners[4]).liquidate(
+            firstCluster.args.owner,
+            firstCluster.args.operatorIds,
+            firstCluster.args.cluster
+        );
+
+        expect((await ssvNetworkViews.getNetworkValidatorsCount())).to.equal(0);
+    });
+
 });
