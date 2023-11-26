8f14127329c87bb197789c2a0e3dd67c109bd3ef
y0sher <lyosher@gmail.com>
2023-11-27 00:59:58 +0200

    only allow devnet on this branch

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -39,14 +39,12 @@ const config: HardhatUserConfig = {
     ],
   },
   networks: {
-    ganache: {
-      chainId: 1337,
-      url: "http://127.0.0.1:8585",
-      ssvToken: process.env.SSVTOKEN_ADDRESS // if empty, deploy SSV mock token
-    } as SSVNetworkConfig,
-    hardhat: {
-      allowUnlimitedContractSize: true
-    }
+    devnet: {
+      url: process.env.DEVNET_ETH_NODE_URL,
+      accounts: [`0x${process.env.DEVNET_OWNER_PRIVATE_KEY}`],
+      gasPrice: +(process.env.GAS_PRICE || ''),
+      gas: +(process.env.GAS || ''),
+    } as SSVNetworkConfig
   },
   etherscan: {
     // Your API key for Etherscan
@@ -69,74 +67,5 @@ const config: HardhatUserConfig = {
     gasPrice: 0.3
   }
 };
-
-if (process.env.GOERLI_ETH_NODE_URL) {
-  const sharedConfig = {
-    url: process.env.GOERLI_ETH_NODE_URL,
-    accounts: [`0x${process.env.GOERLI_OWNER_PRIVATE_KEY}`],
-    gasPrice: +(process.env.GAS_PRICE || ''),
-    gas: +(process.env.GAS || ''),
-  };
-  //@ts-ignore
-  config.networks = {
-    ...config.networks,
-    goerli_development: {
-      ...sharedConfig,
-      ssvToken: '0x6471F70b932390f527c6403773D082A0Db8e8A9F'
-    } as SSVNetworkConfig,
-    goerli_testnet: {
-      ...sharedConfig,
-      ssvToken: '0x3a9f01091C446bdE031E39ea8354647AFef091E7'
-    } as SSVNetworkConfig,
-  }
-}
-
-if (process.env.HOLESKY_ETH_NODE_URL) {
-  const sharedConfig = {
-    url: process.env.HOLESKY_ETH_NODE_URL,
-    accounts: [`0x${process.env.HOLESKY_OWNER_PRIVATE_KEY}`],
-    gasPrice: +(process.env.GAS_PRICE || ''),
-    gas: +(process.env.GAS || ''),
-  };
-  //@ts-ignore
-  config.networks = {
-    ...config.networks,
-    holesky_development: {
-      ...sharedConfig,
-      ssvToken: '0x68A8DDD7a59A900E0657e9f8bbE02B70c947f25F'
-    } as SSVNetworkConfig,
-    holesky_testnet: {
-      ...sharedConfig,
-      ssvToken: '0xad45A78180961079BFaeEe349704F411dfF947C6'
-    } as SSVNetworkConfig,
-  }
-}
-
-if (process.env.MAINNET_ETH_NODE_URL) {
-  //@ts-ignore
-  config.networks = {
-    ...config.networks,
-    mainnet: {
-      url: process.env.MAINNET_ETH_NODE_URL,
-      accounts: [`0x${process.env.MAINNET_OWNER_PRIVATE_KEY}`],
-      gasPrice: +(process.env.GAS_PRICE || ''),
-      gas: +(process.env.GAS || ''),
-      ssvToken: '0x9D65fF81a3c488d585bBfb0Bfe3c7707c7917f54'
-    } as SSVNetworkConfig
-  }
-}
-
-if (process.env.DEVNET_ETH_NODE_URL) {
-  //@ts-ignore
-  config.networks = {
-    ...config.networks,
-    devnet: {
-      url: process.env.DEVNET_ETH_NODE_URL,
-      accounts: [`0x${process.env.DEVNET_OWNER_PRIVATE_KEY}`],
-      gasPrice: +(process.env.GAS_PRICE || ''),
-      gas: +(process.env.GAS || ''),
-    } as SSVNetworkConfig
-  }
-}
 
 export default config;
