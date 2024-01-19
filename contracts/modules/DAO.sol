a400ab60fd771bb7ebec39478a57e54b4e497134
Mohsen-T <mohsentbusiness@gmail.com>
2024-01-19 10:14:09 +0100

    fix: contracts

--- contracts/modules/DAO.sol
+++ contracts/modules/DAO.sol
@@ -4,9 +4,6 @@ pragma solidity 0.8.18;
 import "./SSVDAO.sol";
 
 contract DAO is SSVDAO {
-    uint64 private constant MINIMAL_LIQUIDATION_THRESHOLD = 100_800;
-    bool private _assetVar;
-
     constructor() {
         StorageProtocol storage sp = SSVStorageProtocol.load();
         sp.networkFee = 100000000 / 10;
