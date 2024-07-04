59abd6b1813f50fccb605e7004c638a6a873115c
Marco Tabasco <matabasco@gmail.com>
2024-07-04 17:56:38 +0200

    unlock pragma for interfaces

--- contracts/interfaces/ISSVNetwork.sol
+++ contracts/interfaces/ISSVNetwork.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity 0.8.24;
+pragma solidity ^0.8.20;
 
 import {ISSVNetworkCore} from "./ISSVNetworkCore.sol";
 import {ISSVOperators} from "./ISSVOperators.sol";
