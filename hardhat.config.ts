eaeefdee2781844c673ac5da1e3692d587a7e7fd
y0sher <lyosher@gmail.com>
2025-02-18 20:01:05 +0200

    hh cfg

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -25,12 +25,15 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: '0.8.20',
+        version: '0.8.4',
+      },
+      {
+        version: '0.8.24',
         settings: {
-          optimizer: {
-            enabled: true,
-            runs: 10000,
-          },
+          // optimizer: {
+          //   enabled: true,
+          //   runs: 10000,
+          // },
           evmVersion: 'cancun',
         },
       },
