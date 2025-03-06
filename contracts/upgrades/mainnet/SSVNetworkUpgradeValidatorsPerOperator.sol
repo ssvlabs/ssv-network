008f6752a1cf78334c6d44508e98fa5f5f8abade
Marco Tabasco <matabasco@gmail.com>
2025-03-06 12:01:36 +0100

    fix upgrade

--- contracts/upgrades/mainnet/SSVNetworkUpgradeValidatorsPerOperator.sol
+++ contracts/upgrades/mainnet/SSVNetworkUpgradeValidatorsPerOperator.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.24;
 import "../../SSVNetwork.sol";
 
 contract SSVNetworkUpgradeValidatorsPerOperator is SSVNetwork {
-    function initializev2(uint32 validatorsPerOperatorLimit_) external reinitializer(_getInitializedVersion() + 1) {
+    function initializev2(uint32 validatorsPerOperatorLimit_) external onlyOwner  {
         SSVStorageProtocol.load().validatorsPerOperatorLimit = validatorsPerOperatorLimit_;
     }
 }
