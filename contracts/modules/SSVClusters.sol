5fcdc897e67760b7f09097f15bdd80bc3c45711c
yuriissv <yurii@ssvlabs.io>
2025-12-16 12:39:49 +0100

    feat: handle receiving eb balance in gwei

--- contracts/modules/SSVClusters.sol
+++ contracts/modules/SSVClusters.sol
@@ -536,7 +536,7 @@ contract SSVClusters is ISSVClusters {
             ctx.clusterOwner,
             operatorIds,
             ctx.blockNum,
-            ctx.effectiveBalance,
+            ctx.effectiveBalance * 1 ether,
             newVUnits,
             cluster
         );
