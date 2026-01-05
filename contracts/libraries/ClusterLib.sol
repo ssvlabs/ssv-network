641a0d5f462ac15504c021af8013d80fc838365e
yuriissv <yurii@ssvlabs.io>
2026-01-05 11:13:16 +0100

    hotfix: cluster override

--- contracts/libraries/ClusterLib.sol
+++ contracts/libraries/ClusterLib.sol
@@ -116,6 +116,13 @@ library ClusterLib {
         hashedCluster = keccak256(abi.encodePacked(owner, operatorIds));
 
         bytes32 clusterData = s.ethClusters[hashedCluster];
+        bytes32 clusterDataSSV = s.ethClusters[hashedCluster];
+
+        // todo owner can override ssv cluster here, refactor this check
+        if (clusterData == bytes32(0) && clusterDataSSV!= bytes32(0)) {
+            revert ISSVNetworkCore.IncorrectClusterVersion();
+        }
+
         if (clusterData == bytes32(0)) {
             if (
                 cluster.validatorCount != 0 ||
