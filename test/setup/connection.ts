d9a3e9cf2ccb0dbe34132c873bd732c78733c7a1
yuriissv <yurii@ssvlabs.io>
2026-01-05 11:22:52 +0100

    feat: add types for connection

--- test/setup/connection.ts
+++ test/setup/connection.ts
@@ -1,7 +1,13 @@
 import hre from "hardhat";
+import type { NetworkConnection } from "hardhat/types/network";
+import type { NetworkHelpersType } from "../common/types.ts";
 
-export async function getTestConnection() {
-  const connection = await hre.network.connect();
+export async function getTestConnection(): Promise<{
+  connection: NetworkConnection<"generic">;
+  ethers: NetworkConnection<"generic">["ethers"];
+  networkHelpers: NetworkHelpersType;
+}> {
+  const connection = await hre.network.connect("hardhat");
 
   return {
     connection,
