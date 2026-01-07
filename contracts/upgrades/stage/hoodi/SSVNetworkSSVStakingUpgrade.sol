9682ee5499b967bfd549cd4802a41e9c5900b83a
yuriissv <yurii@ssvlabs.io>
2026-01-07 12:54:23 +0100

    feat: add oracleIds to the initializer

--- contracts/upgrades/stage/hoodi/SSVNetworkSSVStakingUpgrade.sol
+++ contracts/upgrades/stage/hoodi/SSVNetworkSSVStakingUpgrade.sol
@@ -4,12 +4,13 @@ pragma solidity 0.8.24;
 import "../../../SSVNetwork.sol";
 
 contract SSVNetworkSSVStakingUpgrade is SSVNetwork {
-    function initializeSSVStaking(address cssv_, uint64 cooldownDuration_) external onlyOwner reinitializer(2) {
+    function initializeSSVStaking(address cssv_, uint64 cooldownDuration_, uint32[4] memory defaultOracleIds) external onlyOwner reinitializer(2) {
         if (cssv_ == address(0)) revert ZeroAddress();
         
         StorageStaking storage s = SSVStorageStaking.load();
         s.cssv = cssv_;
         s.cooldownDuration = cooldownDuration_;
+        s.defaultOracleIds = defaultOracleIds;
 
         emit CooldownDurationUpdated(cooldownDuration_);
     }
