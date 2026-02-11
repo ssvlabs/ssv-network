645ca907ae9eda8463ec63f1bbfb81fe6312648d
Venimir Petkov <venimir@ssvlabs.io>
2026-02-11 15:36:45 +0100

    fix(liquidation): prevent uint64 overflow in ETH liquidation threshold

--- contracts/libraries/ClusterLib.sol
+++ contracts/libraries/ClusterLib.sol
@@ -8,7 +8,7 @@ import {DEFAULT_EB_PER_VALIDATOR, SSVStorageEB, StorageEB, VUNITS_PRECISION} fro
 import "./OperatorLib.sol";
 import "./ProtocolLib.sol";
 import {PackedSSV, PackedETH, VERSION_SSV, VERSION_ETH} from "../libraries/SSVCoreTypes.sol";
-import {PackedSSVLib, PackedETHLib} from "../libraries/SSVPackedLib.sol";
+import {PackedSSVLib, PackedETHLib, ETH_DEDUCTED_DIGITS} from "../libraries/SSVPackedLib.sol";
 
 /**
  * @title SSV Cluster Library
@@ -78,10 +78,9 @@ library ClusterLib {
         uint64 vUnits = getVUnits(clusterId, cluster.validatorCount);
         uint128 units = vUnits;
         uint128 rate = burnRate + networkFee;
-        uint128 thresholdUnits = (uint128(minimumBlocksBeforeLiquidation) * rate * units) / VUNITS_PRECISION;
-
-        uint64 liquidationThreshold = uint64(thresholdUnits);
-        return cluster.balance < PackedETHLib.unpack(PackedETH.wrap(liquidationThreshold));
+        uint256 thresholdUnits = (uint256(minimumBlocksBeforeLiquidation) * rate * units) / VUNITS_PRECISION;
+        uint256 liquidationThreshold = thresholdUnits * ETH_DEDUCTED_DIGITS;
+        return cluster.balance < liquidationThreshold;
     }
 
     /**
@@ -107,10 +106,9 @@ library ClusterLib {
 
         uint128 units = vUnits;
         uint128 rate = burnRate + networkFee;
-        uint128 thresholdUnits = (uint128(minimumBlocksBeforeLiquidation) * rate * units) / VUNITS_PRECISION;
-
-        uint64 liquidationThreshold = uint64(thresholdUnits);
-        return cluster.balance < PackedETHLib.unpack(PackedETH.wrap(liquidationThreshold));
+        uint256 thresholdUnits = (uint256(minimumBlocksBeforeLiquidation) * rate * units) / VUNITS_PRECISION;
+        uint256 liquidationThreshold = thresholdUnits * ETH_DEDUCTED_DIGITS;
+        return cluster.balance < liquidationThreshold;
     }
 
     /**
