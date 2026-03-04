963a13aa1f1cad93c8342858a149269bcb9726ea
Marco Tabasco <marco@ssvlabs.io>
2026-03-04 14:42:40 +0100

    chore: allow 0 blocks, improve config scripts

--- contracts/modules/SSVDAO.sol
+++ contracts/modules/SSVDAO.sol
@@ -251,8 +251,6 @@ contract SSVDAO is ISSVDAO, SSVReentrancyGuard {
      * @inheritdoc ISSVDAO
      */
     function updateMinBlocksBetweenUpdates(uint32 blocks) external override {
-        if (blocks == 0) revert ZeroAmount();
-
         SSVStorageEB.load().minBlocksBetweenUpdates = blocks;
         emit MinBlocksBetweenUpdatesUpdated(blocks);
     }
